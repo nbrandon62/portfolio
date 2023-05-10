2c7a826aec04abce14b05f9defec15291f70a3ed
nbrandon62 <nbrandon62@gmail.com>
2023-05-10 12:47:17 -0700

    added a PDF viewer to and can navigate there now

--- personal/src/components/ResumePDF.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ResumePDF = () => {
-  return (
-    <div>ResumePDF</div>
-  )
-}
-
-export default ResumePDF
--- personal/src/pages/ResumePage.jsx
+++ personal/src/pages/ResumePage.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ResumePDF from '../components/Resume'
+import { useState } from 'react';
 
-const ResumePage = () => {
+// Import Worker
+import { Worker } from '@react-pdf-viewer/core';
+// Import the main Viewer component
+import { Viewer } from '@react-pdf-viewer/core';
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+// default layout plugin
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+// Import styles of default layout plugin
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import resumeportfolio from '../assets/resumeportfolio.pdf';
+
+function ResumePage() {
+  // creating new plugin instance
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  // pdf file onChange state
+  const [pdfFile, setPdfFile] = useState(resumeportfolio);
+
   return (
-    <div><ResumePDF /></div>
-  )
+    <div className='pdf-container'>
+      <div className='viewer'>
+        {/* render this if we have a pdf file */}
+        {pdfFile && (
+          <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+            <Viewer
+              defaultScale={2}
+              fileUrl={resumeportfolio}
+              plugins={[defaultLayoutPluginInstance]}
+            ></Viewer>
+          </Worker>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default ResumePage
+export default ResumePage;
