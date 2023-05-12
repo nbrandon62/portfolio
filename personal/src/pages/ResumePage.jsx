057bc15ea7d1ae9ad1e7362e36e49ef8a1fc60a2
nbrandon62 <nbrandon62@gmail.com>
2023-05-11 19:43:42 -0700

    moved public folder into src to check for deployment

--- personal/src/pages/ResumePage.jsx
+++ personal/src/pages/ResumePage.jsx
@@ -1,14 +1,8 @@
 import { useState } from 'react';
-
-// Import Worker
 import { Worker } from '@react-pdf-viewer/core';
-// Import the main Viewer component
 import { Viewer } from '@react-pdf-viewer/core';
-// Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
-// default layout plugin
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-// Import styles of default layout plugin
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import resumeportfolio from '../assets/resumeportfolio.pdf';
 
