edee8e34ef808135efb781fd4ed15fd98d548232
nbrandon62 <nbrandon62@gmail.com>
2023-06-06 13:56:06 -0700

    mobile friendly adjustments for footer and project card

--- personal/src/components/ProjectCard.jsx
+++ personal/src/components/ProjectCard.jsx
@@ -9,7 +9,7 @@ const ProjectCard = ({ link, img, title, subtitle, tech }) => {
           <div style={{ marginRight: '1rem' }}>
             <img className='project-card-image' src={img} />
           </div>
-          <div>
+          <div className='project-card-body'>
             <div className='project-card-title'>{title}</div>
             <div className='project-card-subtitle'>{subtitle}</div>
             <div className='project-card-subtitle'>{tech}</div>
