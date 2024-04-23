5056369997d6b79b7c92a49841fb37b499ce9577
nbrandon62 <nbrandon62@gmail.com>
2024-04-23 14:40:57 -0700

    resume edit

--- personal/src/components/Exp.jsx
+++ personal/src/components/Exp.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Grid, Menu } from 'semantic-ui-react';
-import ExpBody from './ExpBody';
+import React, { Component } from 'react'
+import { Grid, Menu } from 'semantic-ui-react'
+import ExpBody from './ExpBody'
 
 const hexigonBody = [
   {
@@ -19,11 +19,38 @@ const hexigonBody = [
     point:
       'Organized and led communication to founders about project updates, challenges, channels of communication, and future road mapping for key features and projects like:',
   },
-];
+]
+const paypalBody = [
+  {
+    point:
+      'Lifted the collocation of GraphQL  loading states from separate components to the Context level in order to remove multiple loading states per component. This renders a single loading spinner while UI updates/ data is fetched. ',
+  },
+  {
+    point:
+      'Supported a11y efforts to 20% of 400 million Paypal users by applying CSS unit best practices across shared UI components.',
+  },
+  {
+    point:
+      'Contributed to bespoke component library by configuring Button component API to cycle through multiple colors without repeats if the developer so chooses, else just applies a single color. ',
+  },
+  {
+    point:
+      'Documented, tested, and shipped changes to PayPal Component Explorer for other developers to discover, and use new component API features. ',
+  },
+  {
+    point:
+      'Refactored unit tests from plain JavaScript to TypeScript to support efforts in migrating the entire testing library over to TypeScript',
+  },
+  {
+    point:
+      'Collaborated with PayPal-React SDK community to read, listen, and implement community changes with the SDK team. Upon approval, implemented new props to Component API per community suggestion.',
+  },
+]
 
 const bitwiseBody = [
   {
-    point: 'Completed client project 1 month before deadline allowing for app optimization, and client edits.',
+    point:
+      'Completed client project 1 month before deadline allowing for app optimization, and client edits.',
   },
   {
     point:
@@ -37,79 +64,90 @@ const bitwiseBody = [
     point:
       'Abstracted rerendering logic to increase readability, and maintainability. This  also removed the need for comments explaining the code.',
   },
-];
+]
 
 export default class Exp extends Component {
-  state = { activeItem: 'Rantir AI', isVertical: false };
+  state = { activeItem: 'Rantir AI', isVertical: false }
 
   componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-    this.handleResize();
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize()
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
+    window.removeEventListener('resize', this.handleResize)
   }
 
   handleResize = () => {
     if (window.innerWidth < 850) {
-      this.setState({ isVertical: false });
+      this.setState({ isVertical: false })
     } else {
-      this.setState({ isVertical: true });
+      this.setState({ isVertical: true })
     }
-  };
+  }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
-    const { activeItem, isVertical } = this.state;
+    const { activeItem, isVertical } = this.state
 
     return (
       <>
-      <div className='exp-container'>
-        <div className='exp-wrapper'>
-          <div className='exp-menu'>
-            <Menu
-              pointing
-              secondary
-              {...(isVertical ? { vertical: true } : {})}
-            >
-              <Menu.Item
-                name='Rantir AI'
-                active={activeItem === 'Rantir AI'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name='Bitwise Industries'
-                active={activeItem === 'Bitwise Industries'}
-                onClick={this.handleItemClick}
-              />
-            </Menu>
-          </div>
-          <div className='exp-text'>
-            {this.state.activeItem === 'Rantir AI' ? (
-              <ExpBody
-                title={'Frontend Engineer'}
-                link={'Rantir AI'}
-                href={'https://www.rantir.com/'}
-                date={'Jan - Jun 2023'}
-                body={hexigonBody}
-              />
-            ) : this.state.activeItem === 'Bitwise Industries' ? (
-              <ExpBody
-                title={'Web Developer'}
-                link={'Bitwise Industries'}
-                // href={'https://bitwiseindustries.com/'}
-                date={'Nov 2021 - Jan 2023'}
-                body={bitwiseBody}
-              />
-            ) : (
-              <div>Invalid company name</div>
-            )}
+        <div className='exp-container'>
+          <div className='exp-wrapper'>
+            <div className='exp-menu'>
+              <Menu
+                pointing
+                secondary
+                {...(isVertical ? { vertical: true } : {})}
+              >
+                <Menu.Item
+                  name='Rantir AI'
+                  active={activeItem === 'Rantir AI'}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name='PayPal'
+                  active={activeItem === 'PayPal'}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name='Bitwise Industries'
+                  active={activeItem === 'Bitwise Industries'}
+                  onClick={this.handleItemClick}
+                />
+              </Menu>
+            </div>
+            <div className='exp-text'>
+              {this.state.activeItem === 'Rantir AI' ? (
+                <ExpBody
+                  title={'Frontend Engineer'}
+                  link={'Rantir AI'}
+                  href={'https://www.rantir.com/'}
+                  date={'Jan - Jun 2023'}
+                  body={hexigonBody}
+                />
+              ) : this.state.activeItem === 'PayPal' ? (
+                <ExpBody
+                  title={'Frontend Engineer'}
+                  link={'PayPal'}
+                  href={'https://www.paypal.com//'}
+                  date={'Dec 2021 - Dec 2022'}
+                  body={paypalBody}
+                />
+              ) : (
+                <ExpBody
+                  title={'Web Developer'}
+                  link={'Bitwise Industries'}
+                  // href={'https://bitwiseindustries.com/'}
+                  date={'Feb 2020 - Nov 2021'}
+                  body={bitwiseBody}
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
       </>
-    );
+    )
   }
 }
